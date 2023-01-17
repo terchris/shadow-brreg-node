ac552eb30d042d47b3891852057dfbe88adafc40
Terje Christensen <terchris@users.noreply.github.com>
2023-01-17 20:54:46 +0100

    ChatGPT is suggestions is out of context

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,24 @@ function displayRecords(records: any[]) {
     })));
 }
 
+async function getLargestOrganizations(numberOfOrganizations: number) {
+    let client = await connect();
+    try {
+        let results = await client.query(`SELECT organisasjonsnummer, navn, hjemmeside, antall_ansatte FROM brreg_enheter_alle ORDER BY antall_ansatte DESC LIMIT $1`, [numberOfOrganizations]);
+        displayRecords(results.rows);
+        return results.rows;
+    } finally {
+        client.release();
+    }
+}
+
+
 
 async function main() {
     let records = await getFirstRecords();
     displayRecords(records);
+    let largestOrganizations = await getLargestOrganizations(5);
+
 }
 console.log("Starting")
 main();

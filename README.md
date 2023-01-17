df6d4920cd6b5e43d290ba5954d851a0adae6dc5
Terje Christensen <terchris@users.noreply.github.com>
2023-01-18 00:42:05 +0100

    ChatGPT is quite good at sql

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # shadow-brreg-node
 typescript/node code for accessing the postgres shadow copy of brreg.no (brønnøysundregistrene) 
 
+
+
 I have used ChatGPT to set up development environment, getting debugging in vscode working, typescript working. All code is written by ChatGPT.
 
 I have added all my questions and ChatGPT's answers to the document charGPT.MD
-
 As I "talk" to ChatGPT and develop the code I do commit so that it is possible to see how ChatGPT has guided my coding. 
 
+## how to play
+To play with we need lots of data. I have created a docker image that download and creates a database that has all companies and organizations in Norway. About a milion (small country) organizations in the database.
+https://github.com/terchris/shadow-brreg
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ function displayRecords(records: any[]) {
         "Navn": record.navn,
         "Hjemmeside": record.hjemmeside,
         "Antall ansatte": record.antall_ansatte,
-        "count": record.count
+        "count": record.count,
+        "duplicate_hjemmeside": record.duplicate_hjemmeside
     })));
 }
 
@@ -30,6 +31,16 @@ async function main() {
     const largestOrganizationsQuery = `SELECT organisasjonsnummer, navn, hjemmeside, antall_ansatte FROM brreg_enheter_alle ORDER BY antall_ansatte DESC`;
     const largestOrganizationsNoWebQuery = `SELECT organisasjonsnummer, navn, hjemmeside, antall_ansatte FROM brreg_enheter_alle WHERE (hjemmeside IS NULL OR hjemmeside = '') ORDER BY antall_ansatte DESC`;
     const duplicateWebQuery = `SELECT hjemmeside, COUNT(*) as count FROM brreg_enheter_alle WHERE hjemmeside IS NOT NULL AND hjemmeside != '' GROUP BY hjemmeside ORDER BY count DESC`;
+    const duplicateWebCountQuery = `SELECT COUNT(hjemmeside) as count, hjemmeside FROM brreg_enheter_alle GROUP BY hjemmeside HAVING COUNT(hjemmeside) > 1 ORDER BY count DESC`;
+    //const duplicateWebTableQuery = `SELECT count, COUNT(count) as frequency FROM (SELECT hjemmeside, COUNT(hjemmeside) as count FROM brreg_enheter_alle GROUP BY hjemmeside HAVING count > 1) subquery GROUP BY count ORDER BY count DESC`;
+
+    const duplicateWebTableQuery = `SELECT COUNT(*) as count, duplicate_hjemmeside FROM (
+        SELECT COUNT(hjemmeside) as duplicate_hjemmeside 
+        FROM brreg_enheter_alle 
+        GROUP BY hjemmeside
+    ) subquery 
+    GROUP BY duplicate_hjemmeside
+    ORDER BY duplicate_hjemmeside DESC`;
 
 
     console.log("Getting first records in the table");
@@ -47,6 +58,14 @@ async function main() {
     console.log("Getting duplicate websites");
     const duplicateWeb = await getOrganizations(duplicateWebQuery, numberOfOrganizations);
     displayRecords(duplicateWeb);
+
+    console.log("Getting duplicate websites count");
+    const duplicateWebCount = await getOrganizations(duplicateWebCountQuery, numberOfOrganizations);
+    displayRecords(duplicateWebCount);
+
+    console.log("Getting duplicate websites table");
+    const duplicateWebTable = await getOrganizations(duplicateWebTableQuery, 10000);
+    displayRecords(duplicateWebTable);
 
 }
 
